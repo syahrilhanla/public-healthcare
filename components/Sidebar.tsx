da2a726a3902a4f217902d01d88b9eee04483e14
syahrilhanla <syahrilhanla5@gmail.com>
2024-04-02 12:05:19 +0700

    chore: add sidebarLinks file to lib directory to be reused

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,16 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
-import {
-  BarChart2,
-  BookMarked,
-  ClipboardPlus,
-  HeartPulseIcon,
-  Speech,
-  UserCog,
-  Users
-} from "lucide-react";
+import { usePathname } from "next/navigation";
+
+import { sidebarLinks } from "lib/sidebarLinks";
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <div className="hidden bg-muted/40 md:block">
       <div className="flex h-full max-h-screen flex-col gap-2">
@@ -27,55 +23,16 @@ const Sidebar = () => {
         </div>
         <div className="flex-1">
           <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg px-3 py-4 text-muted-foreground transition-all hover:text-primary"
-            >
-              <BarChart2 className="h-5 w-5" />
-              Hasil Pemeriksaan
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg px-3 py-4 text-muted-foreground transition-all hover:text-primary"
-            >
-              <HeartPulseIcon className="h-5 w-5" />
-              Tablet Tambah Darah
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg bg-muted px-3 py-4 text-primary transition-all hover:text-primary"
-            >
-              <Speech className="h-5 w-5" />
-              Konsultasi
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg px-3 py-4 text-muted-foreground transition-all hover:text-primary"
-            >
-              <ClipboardPlus className="h-5 w-5" />
-              Laporan
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg px-3 py-4 text-muted-foreground transition-all hover:text-primary"
-            >
-              <BookMarked className="h-5 w-5" />
-              Materi Penyuluhan
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg px-3 py-4 text-muted-foreground transition-all hover:text-primary"
-            >
-              <Users className="h-5 w-5" />
-              Profil
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg px-3 py-4 text-muted-foreground transition-all hover:text-primary"
-            >
-              <UserCog className="h-5 w-5" />
-              Pengaturan Admin
-            </Link>
+            {sidebarLinks.map((link, index) => (
+              <Link
+                key={index}
+                href={link.href}
+                className={`flex items-center gap-3 rounded-lg px-3 py-4 text-muted-foreground transition-all hover:text-primary ${pathname === link.href ? 'bg-muted' : ''}`}
+              >
+                {link.icon}
+                {link.text}
+              </Link>
+            ))}
           </nav>
         </div>
       </div>
---
+++ lib/sidebarLinks.tsx
@@ -0,0 +1,19 @@
+import {
+  BarChart2,
+  HeartPulseIcon,
+  Speech,
+  ClipboardPlus,
+  BookMarked,
+  Users,
+  UserCog
+} from "lucide-react";
+
+export const sidebarLinks = [
+  { href: '/dashboard/hasil-pemeriksaan', text: 'Hasil Pemeriksaan', icon: <BarChart2 className="h-5 w-5" /> },
+  { href: '/dashboard/ttd', text: 'Tablet Tambah Darah', icon: <HeartPulseIcon className="h-5 w-5" /> },
+  { href: '/dashboard/konsultasi', text: 'Konsultasi', icon: <Speech className="h-5 w-5" /> },
+  { href: '/dashboard/laporan', text: 'Laporan', icon: <ClipboardPlus className="h-5 w-5" /> },
+  { href: '/dashboard/materi', text: 'Materi Penyuluhan', icon: <BookMarked className="h-5 w-5" /> },
+  { href: '/dashboard/profil', text: 'Profil', icon: <Users className="h-5 w-5" /> },
+  { href: '/dashboard/pengaturan', text: 'Pengaturan Admin', icon: <UserCog className="h-5 w-5" /> }
+];
