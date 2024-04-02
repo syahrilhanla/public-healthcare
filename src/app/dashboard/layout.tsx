57e4526e38851b3cac84a42295447774060ea365
syahrilhanla <syahrilhanla5@gmail.com>
2024-04-02 10:04:18 +0700

    feat: add layout file for dashboard path

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Sidebar from "../../../components/Sidebar";
+import Navbar from "../../../components/Navbar";
+import Snackbar from "../../../components/Snackbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+          <Sidebar />
+          <div className="flex flex-col">
+            <Navbar />
+            <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6">
+              {children}
+            </main>
+          </div>
+          <Snackbar />
+        </div>
+      </body>
+
+    </html>
+  );
+}
