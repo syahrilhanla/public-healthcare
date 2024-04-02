9966f3357f9f7257d397bc0fcbea27fe77acee1e
syahrilhanla <syahrilhanla5@gmail.com>
2024-04-02 10:40:20 +0700

    feat: create MobileSidebar component

---
+++ components/MobileSidebar.tsx
@@ -0,0 +1,83 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { BarChart2, BookMarked, ClipboardPlus, HeartPulseIcon, Menu, Speech, Users } from "lucide-react";
+
+const MobileSidebar = () => {
+  return (
+    <>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button
+            variant="outline"
+            size="icon"
+            className="shrink-0 md:hidden"
+          >
+            <Menu className="h-5 w-5" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="flex flex-col max-w-[70dvw]">
+          <nav className="grid gap-2 text-lg font-medium">
+            <Link
+              href="#"
+              className="flex items-center gap-2 text-lg font-semibold"
+            >
+              <Image
+                src="/img/logo.png"
+                alt="Workflow"
+                width={40}
+                height={40}
+              />
+            </Link>
+            <Link
+              href="#"
+              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+            >
+              <BarChart2 className="h-4 w-4" />
+              Hasil Pemeriksaan
+            </Link>
+            <Link
+              href="#"
+              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
+            >
+              <HeartPulseIcon className="h-4 w-4" />
+              Tablet Tambah Darah
+            </Link>
+            <Link
+              href="#"
+              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+            >
+              <Speech className="h-4 w-4" />
+              Konsultasi
+            </Link>
+            <Link
+              href="#"
+              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+            >
+              <ClipboardPlus className="h-4 w-4" />
+              Laporan
+            </Link>
+            <Link
+              href="#"
+              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+            >
+              <BookMarked className="h-4 w-4" />
+              Materi Penyuluhan
+            </Link>
+            <Link
+              href="#"
+              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+            >
+              <Users className="h-4 w-4" />
+              Profil
+            </Link>
+          </nav>
+        </SheetContent>
+      </Sheet>
+    </>
+  )
+}
+
+export default MobileSidebar
