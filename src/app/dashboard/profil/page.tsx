793269965490614f7e500d83bf52211f4e8ac289
syahrilhanla <syahrilhanla5@gmail.com>
2024-04-15 22:05:03 +0700

    feat: add UI for profile list page table

--- src/app/dashboard/profil/page.tsx
+++ src/app/dashboard/profil/page.tsx
@@ -1,6 +1,46 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 const ProfileList = () => {
   return (
-    <div>Profile List</div>
+    <div className="grid gap-4 px-2 lg:px-6">
+      <h1
+        className="text-2xl font-semibold text-gray-600"
+      >
+        Profil
+      </h1>
+
+      <Table>
+        <TableHeader>
+          <TableRow className="overflow-auto font-medium">
+            <TableHead className="w-[200px] font-medium text-black">Nama</TableHead>
+            <TableHead className="font-medium text-black">JK</TableHead>
+            <TableHead className="font-medium text-black">Sekolah</TableHead>
+            <TableHead className="font-medium text-black">TTL</TableHead>
+            <TableHead className="text-right font-medium text-black">NIK</TableHead>
+            <TableHead className="font-medium text-black">Posyandu</TableHead>
+            <TableHead className="font-medium text-black">Alamat</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody >
+          <TableRow>
+            <TableCell className="text-gray-600">Test 1</TableCell>
+            <TableCell className="text-gray-600 w-[7rem]">Perempuan</TableCell>
+            <TableCell className="text-gray-600">SMA 2 Banjarmasin</TableCell>
+            <TableCell className="text-gray-600">Banjarmasin, 12 Maret 2009</TableCell>
+            <TableCell className="text-gray-600">2918273756448882</TableCell>
+            <TableCell className="text-gray-600">Posyandu Remaja FRESH</TableCell>
+            <TableCell className="text-gray-600">Jl. Pengambangan</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
   )
 }
 
