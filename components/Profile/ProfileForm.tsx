19af2a29689acd020e7421d5b5322f4c150ba822
syahrilhanla <syahrilhanla5@gmail.com>
2024-04-20 21:21:01 +0700

    fix: adjust loading indicator layout in ProfileForm

--- components/Profile/ProfileForm.tsx
+++ components/Profile/ProfileForm.tsx
@@ -43,8 +43,8 @@ const ProfileForm = () => {
 
       {
         formStatus === "loading" ? (
-          <div>
-            <LoaderCircle className="animate-spin" /> Loading...
+          <div className="w-full flex gap-3 justify-center text-center">
+            <LoaderCircle className="animate-spin" /> <p>Loading...</p>
           </div>
         ) : (
           <Form {...form}>
