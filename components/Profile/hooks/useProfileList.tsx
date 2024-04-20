c9dcf1a8055fc3f5d47c13f47bbfa0cedade5c2e
syahrilhanla <syahrilhanla5@gmail.com>
2024-04-20 23:03:28 +0700

    fix: sort profile list by descending

--- components/Profile/hooks/useProfileList.tsx
+++ components/Profile/hooks/useProfileList.tsx
@@ -1,15 +1,18 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
 import { db } from "lib/firebase.sdk";
 import { Profile } from "type/profile.type";
 
 const useProfileList = async () => {
   const profiles: Profile[] = [];
 
-  const profilesResponse = await getDocs(collection(db, "users"));
+  const profileRef = query(collection(db, "users"), orderBy("updatedAt", "desc"));
+
+  const profilesResponse = await getDocs(profileRef);
+
   profilesResponse.docs.map((doc) => {
     const profile = {
-      ...doc.data().data,
-      birthDate: doc.data().data.birthDate.toDate().toDateString()
+      ...doc.data(),
+      birthDate: doc.data().birthDate.toDate().toDateString()
     }
 
     profiles.push(profile as Profile);
