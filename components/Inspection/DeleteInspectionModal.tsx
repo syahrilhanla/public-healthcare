d55a5d6cd0421ff06c08d563454dfc8f1e9b0d3d
syahrilhanla <syahrilhanla5@gmail.com>
2024-04-21 13:17:31 +0700

    feat: create delete inspection modal

---
+++ components/Inspection/DeleteInspectionModal.tsx
@@ -0,0 +1,77 @@
+"use client"
+
+import { doc, deleteDoc } from "firebase/firestore";
+import { db } from "lib/firebase.sdk";
+
+import Modal from "components/Modal";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast";
+import { TrashIcon } from "lucide-react";
+
+import { Inspection } from "type/inspection.type";
+
+interface Props {
+  selectedInspection: Inspection;
+}
+
+const DeleteInspectionModal = ({ selectedInspection }: Props) => {
+  const { toast } = useToast();
+
+  const handleDelete = async () => {
+    try {
+      await deleteDoc(doc(db, "users", selectedInspection.inspectionId));
+
+      toast({
+        description: "Data berhasil dihapus!",
+        variant: "default",
+      });
+    } catch (error) {
+      console.error("Error removing document: ", error);
+
+      toast({
+        description: "Gagal menghapus data!",
+        variant: "destructive",
+        action: <ToastAction
+          altText="Coba lagi"
+          onClick={async () => {
+            await deleteDoc(doc(db, "users", selectedInspection.inspectionId));
+          }}
+        >Coba lagi
+        </ToastAction>,
+      });
+    }
+  }
+
+  return (
+    <Modal
+      Trigger={
+        <Button variant={"destructive"} className="text-gray-600 py-1 px-3">
+          <TrashIcon className="w-3 h-3 text-white" />
+        </Button>
+      }
+      title="Konfirmasi Hapus Data"
+    >
+      <div className="w-full space-y-4 gap-1 flex flex-col justify-end">
+        <p>
+          Apakah anda yakin ingin menghapus data ini?
+        </p>
+        <div className="grid gap-1 text-slate-500">
+          <p>
+            {selectedInspection.name}
+          </p>
+        </div>
+
+        <Button
+          variant={"destructive"}
+          onClick={handleDelete}
+        >
+          Hapus
+        </Button>
+      </div>
+
+    </Modal>
+  )
+}
+
+export default DeleteInspectionModal;
