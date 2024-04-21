df3a9563263439127589f4cc757eaaf272d74333
syahrilhanla <syahrilhanla5@gmail.com>
2024-04-21 11:39:59 +0700

    chore (helpers): add debounce hook to project

---
+++ lib/helpers.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export const generateUID = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+export function useDebounce<T>(value: T, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(handler);
+  }, [value]);
+
+  return debouncedValue;
+}
