17bad286b5770c958f346e0fe561f3330cbe494d
syahrilhanla <syahrilhanla5@gmail.com>
2024-04-21 09:09:39 +0700

    feat: duplicate profile list page to inspection list page

--- src/app/dashboard/hasil-pemeriksaan/page.tsx
+++ src/app/dashboard/hasil-pemeriksaan/page.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import { Link } from "next-view-transitions";
 
-const Page = () => {
+import { Button } from "@/components/ui/button";
+
+import useProfileList from "components/Profile/hooks/useProfileList";
+import ProfileListTable from "components/Profile/ProfileListTable";
+
+import { PlusIcon } from "lucide-react";
+
+const InspectionList = async () => {
+  const profiles = await useProfileList();
+
   return (
-    <div>
-      <p>incaan ja nih</p>
+    <div className="grid gap-4 px-2 lg:px-6">
+      <h1
+        className="text-2xl font-semibold text-gray-600"
+      >
+        Hasil Pemeriksaan
+      </h1>
+
+      <div className="w-full lg:flex lg:justify-end">
+        <Link href={"/dashboard/profil/form"}>
+          <Button variant={"outline"}
+            className="w-full lg:w-fit flex gap-2 rounded-lg text-gray-600"
+          >
+            <PlusIcon className="h-4 w-4" />
+            Tambah Data
+          </Button>
+        </Link>
+      </div>
+
+      <ProfileListTable profiles={profiles} />
     </div>
   )
 }
 
-export default Page
+export default InspectionList;
