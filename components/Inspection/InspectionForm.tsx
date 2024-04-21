59343553f6a23534a53aba89349dc9fc742c363d
syahrilhanla <syahrilhanla5@gmail.com>
2024-04-21 09:20:14 +0700

    feat: duplicate profile form as inspection form

---
+++ components/Inspection/InspectionForm.tsx
@@ -0,0 +1,261 @@
+"use client"
+import 'react-day-picker/dist/style.css';
+
+import ControlledInput from "components/ControlledInput";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { Label } from "@/components/ui/label";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from "@/components/ui/select";
+import { CalendarIcon, LoaderCircle, X } from "lucide-react";
+
+import useInspectionForm from "components/Inspection/hooks/useInpsectionForm";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import FailedIndicator from "components/FailedIndicator";
+
+const ProfileForm = () => {
+  const {
+    form, onSubmit, formStatus
+  } = useInspectionForm();
+
+  return (
+    <div className="grid gap-4 mx-auto lg:w-[70%] text-gray-600 duration-500">
+      <h1
+        className="text-2xl font-semibold text-gray-600"
+      >
+        Profil
+      </h1>
+
+      {
+        formStatus === "loading" ? (
+          <div className="w-full flex gap-3 justify-center text-center">
+            <LoaderCircle className="animate-spin" /> <p>Loading...</p>
+          </div>
+        ) :
+          formStatus === "error" ? (
+            <FailedIndicator />
+          ) : (
+            <Form {...form}>
+              <form className="grid gap-4" onSubmit={form.handleSubmit(onSubmit)}>
+                <ControlledInput
+                  formSchema={form}
+                  inputId="name"
+                  includeError
+                  labelText="Nama Lengkap"
+                  placeholder="Nama"
+                />
+
+                <ControlledInput
+                  formSchema={form}
+                  inputId="nik"
+                  includeError
+                  labelText="NIK"
+                  placeholder="NIK"
+                  type="number"
+                />
+
+                <div className="space-y-2">
+                  <div className="grid grid-cols-[5fr_5fr] gap-4">
+                    <ControlledInput
+                      formSchema={form}
+                      inputId="birthPlace"
+                      includeError
+                      labelText="Tempat Lahir, Tanggal Lahir"
+                      placeholder="Tempat Lahir"
+                    />
+                    <FormField
+                      control={form.control}
+                      name={"birthDate"}
+                      render={({ field }) => (
+                        <>
+                          <FormItem className="flex flex-col justify-end items-start">
+                            <Popover>
+                              <PopoverTrigger asChild>
+                                <FormControl>
+                                  <Button
+                                    variant={"outline"}
+                                    className={cn(
+                                      "w-full text-left font-normal text-slate-500"
+                                    )}
+                                  >
+                                    {field.value ? (
+                                      format(field.value, "dd MMMM yyyy")
+                                    ) : (
+                                      <span>Tanggal Lahir</span>
+                                    )}
+                                    <CalendarIcon className="ml-auto h-4 w-4 opacity-80" />
+                                  </Button>
+                                </FormControl>
+                              </PopoverTrigger>
+                              <PopoverContent className="w-auto p-0" align="start">
+                                <Calendar
+                                  mode="single"
+                                  selected={field.value}
+                                  onSelect={field.onChange}
+                                  fromYear={1970}
+                                  toYear={new Date().getFullYear()}
+                                  styles={{
+                                    caption_dropdowns: {
+                                      width: "12rem",
+                                      display: "flex",
+                                      gap: "1rem",
+                                      color: "slategrey"
+                                    }
+                                  }}
+                                  captionLayout="dropdown-buttons"
+                                />
+                              </PopoverContent>
+                            </Popover>
+                            <FormMessage />
+                          </FormItem>
+                        </>
+                      )}
+                    />
+                  </div>
+                </div>
+
+                <FormField
+                  control={form.control}
+                  name={"sex"}
+                  render={({ field }) => (
+                    <>
+                      <FormItem className="space-y-2">
+                        <FormLabel htmlFor="sex">Jenis Kelamin</FormLabel>
+                        <RadioGroup
+                          className="flex gap-6 duration-500"
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <FormItem className="space-x-2 flex items-center">
+                            <Label htmlFor="male" className="space-x-2">
+                              <FormControl>
+                                <RadioGroupItem
+                                  value="Laki-laki"
+                                  id="male"
+                                  checked={field.value === "Laki-laki"}
+                                />
+                              </FormControl>
+                              <span>
+                                Laki-laki
+                              </span>
+                            </Label>
+                          </FormItem>
+                          <FormItem className="space-x-2 flex items-center">
+                            <Label htmlFor="female" className="space-x-2">
+                              <FormControl>
+                                <RadioGroupItem
+                                  value="Perempuan"
+                                  id="female"
+                                  checked={field.value === "Perempuan"}
+                                />
+                              </FormControl>
+                              <span>
+                                Perempuan
+                              </span>
+                            </Label>
+                          </FormItem>
+                        </RadioGroup>
+                        <FormMessage />
+                      </FormItem>
+                    </>
+                  )}
+                />
+
+                <ControlledInput
+                  formSchema={form}
+                  inputId="address"
+                  includeError
+                  labelText="Alamat"
+                  placeholder="Alamat"
+                  type="textarea"
+                />
+
+                <ControlledInput
+                  formSchema={form}
+                  inputId="school"
+                  includeError
+                  labelText="Sekolah"
+                  placeholder="Asal Sekolah"
+                />
+
+                <FormField
+                  control={form.control}
+                  name={"posyandu"}
+                  render={({ field }) => (
+                    <>
+                      <FormItem>
+                        <div className="space-y-2 grid">
+                          <FormLabel htmlFor="posyandu">Posyandu</FormLabel>
+                          <FormControl>
+                            <Select
+                              onValueChange={field.onChange}
+                              defaultValue={field.value}
+                            >
+                              <SelectTrigger
+                                id="posyandu"
+                                className="w-full p-2 border border-gray-300 rounded-md 
+                            focus:outline-none focus:ring-0 focus:ring-transparent"
+                              >
+                                <SelectValue
+                                  placeholder={field.value || "Pilih Posyandu"}
+                                  className="w-full space-x-2 focus-visible:ring-transparent"
+                                />
+                              </SelectTrigger>
+                              <SelectContent>
+                                <SelectItem value="Posyandu Remaja FRESH">
+                                  Posyandu Remaja FRESH
+                                </SelectItem>
+                                <SelectItem value="Posyandu Remaja Smart Gemilang">
+                                  Posyandu Remaja Smart Gemilang
+                                </SelectItem>
+                                <SelectItem value="Posyandu Remaja Kusuma Jaya">
+                                  Posyandu Remaja Kusuma Jaya
+                                </SelectItem>
+                                <SelectItem value="Posyandu Remaja Mandiri Sehat">
+                                  Posyandu Remaja Mandiri Sehat
+                                </SelectItem>
+                              </SelectContent>
+                            </Select>
+                          </FormControl>
+                          <FormMessage />
+                        </div>
+                      </FormItem>
+                    </>
+                  )}
+                />
+                <Button
+                  type="submit"
+                >
+                  {
+                    formStatus === "submitting" ? (
+                      <LoaderCircle className="animate-spin" />
+                    ) : (
+                      "Simpan"
+                    )
+                  }
+                </Button>
+              </form>
+            </Form>
+          )
+      }
+    </div>
+  )
+}
+
+export default ProfileForm;
---
+++ components/Inspection/hooks/useInpsectionForm.tsx
@@ -0,0 +1,129 @@
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "lib/firebase.sdk";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { toast } from "@/components/ui/use-toast";
+
+const schema = z.object({
+  name: z.string().min(4, {
+    message: "Masukkan minimal 3 karakter"
+  }),
+  nik: z.string().min(16, {
+    message: "Masukkan NIK yang valid"
+  }).max(16, {
+    message: "Masukkan NIK yang valid"
+  }),
+  birthPlace: z.string().min(1, {
+    message: "Masukkan tempat lahir yang valid"
+  }),
+  birthDate: z.date({
+    required_error: "Masukkan tanggal lahir yang valid",
+    invalid_type_error: "Masukkan tanggal lahir yang valid"
+  }), // "2022-01-01
+  address: z.string().min(1, { message: "Masukkan alamat yang valid" }),
+  school: z.string().min(1, { message: "Masukkan sekolah yang valid" }),
+  posyandu: z.string().min(1, { message: "Pilih posyandu yang valid" }),
+  sex: z.string().min(1, { message: "Pilih jenis kelamin yang valid" }),
+});
+
+type FormStatus = "editing" | "loading" | "submitting" | "error";
+
+const useInspectionForm = () => {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      name: "",
+      address: "",
+      nik: "",
+      birthPlace: "",
+      birthDate: undefined,
+      school: "",
+      posyandu: "",
+      sex: "",
+    },
+  });
+
+  const [formStatus, setFormStatus] = useState<FormStatus>("editing");
+
+  const router = useRouter();
+  const userId = useSearchParams().get("id");
+
+  const getUserData = useCallback(async () => {
+    if (!userId) return;
+
+    try {
+      setFormStatus("loading");
+      const docRef = doc(db, "users", userId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        const data = docSnap.data();
+        const birthDate = new Date(data.birthDate.toDate());
+
+        form.reset({
+          ...data,
+          birthDate
+        });
+      }
+
+      setFormStatus("editing");
+    } catch (error) {
+      console.error("Error to get user data", error);
+      setFormStatus("error");
+    }
+  }, [userId]);
+
+  useEffect(() => {
+    if (userId) {
+      getUserData();
+    }
+  }, [userId, getUserData])
+
+  const onSubmit = async (data: z.infer<typeof schema>) => {
+    // If userId is exist, use userId as reference, otherwise use NIK as reference
+    // used for create or edit user data
+    const reference = userId ? userId : data.nik;
+
+    try {
+      setFormStatus("submitting");
+
+      const profilePayload = {
+        ...data,
+        updatedAt: serverTimestamp()
+      }
+
+      await setDoc(doc(db, "users", reference), {
+        ...profilePayload
+      });
+
+      toast({
+        description: userId ? "Berhasil mengubah profil!" : "Berhasil membuat profil!",
+        variant: "default"
+      });
+
+      router.push("/dashboard/profil");
+    } catch (error) {
+      console.error("Error to create/edit user data", error);
+
+      toast({
+        title: userId ? "gagal mengubah profil!" : "Gagal membuat profil!",
+        description: "Silahkan coba lagi",
+        variant: "destructive"
+      });
+    } finally {
+      setFormStatus("editing");
+    }
+  }
+
+  return {
+    form,
+    onSubmit,
+    formStatus
+  };
+};
+
+export default useInspectionForm;
