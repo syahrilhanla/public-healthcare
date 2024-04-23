d54ba934025ef865b8fa77da65aea15c0164a241
syahrilhanla <syahrilhanla5@gmail.com>
2024-04-23 07:08:32 +0700

    fix: add inspection date to inspection table

--- components/Inspection/InspectionListTable.tsx
+++ components/Inspection/InspectionListTable.tsx
@@ -20,10 +20,11 @@ interface Props {
 
 const InspectionListTable = ({ inspections }: Props) => {
   return (
-    <Table>
+    <Table className="max-w-full">
       <TableHeader>
         <TableRow className="overflow-auto font-medium relative">
           <TableHead className="font-medium text-black">Nama</TableHead>
+          <TableHead className="font-medium text-black">Tanggal</TableHead>
           <TableHead className="text-right font-medium text-black">TB</TableHead>
           <TableHead className="text-right font-medium text-black">BB</TableHead>
           <TableHead className="text-right font-medium text-black">LP</TableHead>
@@ -39,15 +40,16 @@ const InspectionListTable = ({ inspections }: Props) => {
       <TableBody >
         {inspections.map((inspection) => (
           <TableRow key={inspection.inspectionId}>
-            <TableCell className="max-w-[15rem] text-gray-600 text-nowrap truncate">{inspection.name}</TableCell>
-            <TableCell className="text-right text-gray-600 text-nowrap">{inspection.TB}</TableCell>
-            <TableCell className="text-right text-gray-600 text-nowrap">{inspection.BB}</TableCell>
-            <TableCell className="text-right text-gray-600">{inspection.LP}</TableCell>
-            <TableCell className="text-right text-gray-600 text-nowrap">{inspection.LILA}</TableCell>
-            <TableCell className="text-right text-gray-600 text-nowrap">{inspection.Hb}</TableCell>
-            <TableCell className="text-right text-gray-600 text-nowrap">{inspection.TD}</TableCell>
+            <TableCell className="w-[18rem] text-gray-600 text-nowrap truncate">{inspection.name}</TableCell>
+            <TableCell className="w-[10rem] text-gray-600 text-nowrap">{inspection.updatedAt}</TableCell>
+            <TableCell className="w-[5rem] text-right text-gray-600 text-nowrap">{inspection.TB}</TableCell>
+            <TableCell className="w-[5rem] text-right text-gray-600 text-nowrap">{inspection.BB}</TableCell>
+            <TableCell className="w-[5rem] text-right text-gray-600">{inspection.LP}</TableCell>
+            <TableCell className="w-[5rem] text-right text-gray-600 text-nowrap">{inspection.LILA}</TableCell>
+            <TableCell className="w-[5rem] text-right text-gray-600 text-nowrap">{inspection.Hb}</TableCell>
+            <TableCell className="w-[5rem] text-right text-gray-600 text-nowrap">{inspection.TD}</TableCell>
             <TableCell className="max-w-10 text-right text-gray-600 sticky right-0 z-10 bg-white">
-              <div className="flex gap-2 w-fit">
+              <div className="flex gap-2 w-full justify-end">
                 <Link href={`/dashboard/hasil-pemeriksaan/form?id=${inspection.inspectionId}`}>
                   <Button variant={"outline"} className="text-gray-600 py-1 px-3">
                     <PencilIcon className="w-3 h-3" />
