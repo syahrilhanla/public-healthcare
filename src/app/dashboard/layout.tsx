dfaa7fa2500edc81f2dc58c0810b71f00e90904c
syahrilhanla <syahrilhanla5@gmail.com>
2024-04-25 09:12:06 +0700

    fix: remove hydration error caused by cookies checking

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { ViewTransitions } from 'next-view-transitions'
 
@@ -14,15 +14,27 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [loginCookie, setLoginCookie] = useState<undefined | string>("");
+
   const router = useRouter();
   const pathname = usePathname();
-  const loginCookie = typeof window !== "undefined" && window.document ?
-    document.cookie.split('; ').find(row => row.startsWith('login')) : "";
 
   useEffect(() => {
-    if (!loginCookie) router.push("/");
+    if (typeof window !== "undefined") {
+      const cookie = typeof window !== "undefined" && window.document ?
+        document.cookie.split('; ').find(row => row.startsWith('login')) : "";
+
+      setLoginCookie(cookie || undefined);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (loginCookie === undefined) router.push("/");
+  }, [loginCookie]);
+
+  useEffect(() => {
     if (pathname === "/dashboard") router.push("/dashboard/hasil-pemeriksaan");
-  }, [pathname, router, loginCookie]);
+  }, [pathname, router]);
 
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
@@ -33,7 +45,7 @@ export default function RootLayout({
             <MobileSidebar />
           </Navbar>
           <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6">
-            {loginCookie ? children : <></>}
+            {loginCookie ? children : <>no cookie</>}
           </main>
         </div>
         <Snackbar />
