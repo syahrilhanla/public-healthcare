4db2f2a715b4f8e6985e085271e3cd32452b4087
syahrilhanla <syahrilhanla5@gmail.com>
2024-04-27 08:04:46 +0700

    feat: TTD monthly table

--- components/TTD/TTDListTable.tsx
+++ components/TTD/TTDListTable.tsx
@@ -1,7 +1,5 @@
 import { Link } from "next-view-transitions";
 
-import DeleteInspectionModal from "components/Inspection/DeleteInspectionModal";
-import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -10,55 +8,63 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { PencilIcon } from "lucide-react";
+import { Check, X } from "lucide-react";
 
-import { Inspection } from "type/inspection.type";
+import { TtdType } from "type/ttd.type";
 
 interface Props {
-  inspections: Inspection[];
+  TTDs: TtdType[];
 }
 
-const TTDListTable = ({ inspections }: Props) => {
+const TTDListTable = ({ TTDs }: Props) => {
   return (
     <Table className="max-w-full">
       <TableHeader>
         <TableRow className="overflow-auto font-medium relative">
-          <TableHead className="font-medium text-black">Nama</TableHead>
-          <TableHead className="font-medium text-black">Tanggal</TableHead>
-          <TableHead className="text-right font-medium text-black">TB</TableHead>
-          <TableHead className="text-right font-medium text-black">BB</TableHead>
-          <TableHead className="text-right font-medium text-black">LP</TableHead>
-          <TableHead className="text-right font-medium text-black">Lila</TableHead>
-          <TableHead className="text-right font-medium text-black">Hb</TableHead>
-          <TableHead className="text-right font-medium text-black">TD</TableHead>
-          <TableHead
-            className="text-right font-medium text-black sticky right-0 z-10 bg-white">
-            Aksi
+          <TableHead className="font-medium text-black" rowSpan={2}>Nama</TableHead>
+          <TableHead className="font-medium text-black align-middle text-center"
+            rowSpan={1}
+            colSpan={12}
+          >
+            Pemantauan Bulanan
           </TableHead>
+        </TableRow>
+        <TableRow className="overflow-auto font-medium relative">
+          <TableHead className="font-medium text-black" rowSpan={1}>Jan</TableHead>
+          <TableHead className="font-medium text-black" rowSpan={1}>Feb</TableHead>
+          <TableHead className="font-medium text-black" rowSpan={1}>Mar</TableHead>
+          <TableHead className="font-medium text-black" rowSpan={1}>Apr</TableHead>
+          <TableHead className="font-medium text-black" rowSpan={1}>Mei</TableHead>
+          <TableHead className="font-medium text-black" rowSpan={1}>Jun</TableHead>
+          <TableHead className="font-medium text-black" rowSpan={1}>Jul</TableHead>
+          <TableHead className="font-medium text-black" rowSpan={1}>Agu</TableHead>
+          <TableHead className="font-medium text-black" rowSpan={1}>Sep</TableHead>
+          <TableHead className="font-medium text-black" rowSpan={1}>Okt</TableHead>
+          <TableHead className="font-medium text-black" rowSpan={1}>Nov</TableHead>
+          <TableHead className="font-medium text-black" rowSpan={1}>Des</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody >
-        {inspections.map((inspection) => (
-          <TableRow key={inspection.inspectionId}>
-            <TableCell className="w-[18rem] text-gray-600 text-nowrap truncate">{inspection.name}</TableCell>
-            <TableCell className="w-[10rem] text-gray-600 text-nowrap">{inspection.updatedAt}</TableCell>
-            <TableCell className="w-[5rem] text-right text-gray-600 text-nowrap">{inspection.TB}</TableCell>
-            <TableCell className="w-[5rem] text-right text-gray-600 text-nowrap">{inspection.BB}</TableCell>
-            <TableCell className="w-[5rem] text-right text-gray-600">{inspection.LP}</TableCell>
-            <TableCell className="w-[5rem] text-right text-gray-600 text-nowrap">{inspection.LILA || "-"}</TableCell>
-            <TableCell className="w-[5rem] text-right text-gray-600 text-nowrap">{inspection.Hb || "-"}</TableCell>
-            <TableCell className="w-[5rem] text-right text-gray-600 text-nowrap">{inspection.TD}</TableCell>
-            <TableCell className="max-w-10 text-right text-gray-600 sticky right-0 z-10 bg-white">
-              <div className="flex gap-2 w-full justify-end">
-                <Link href={`/dashboard/hasil-pemeriksaan/form?id=${inspection.inspectionId}`}>
-                  <Button variant={"outline"} className="text-gray-600 py-1 px-3">
-                    <PencilIcon className="w-3 h-3" />
-                  </Button>
+        {TTDs.map((TTD) => (
+          <>
+            <TableRow key={TTD.ttdId}>
+              <TableCell className="w-[18rem] text-gray-600 text-nowrap truncate">
+                <Link href={`/dashboard/ttd/form?id=${TTD.nik}`}>
+                  <div className="grid gap-0.5">
+                    {TTD.name}
+                    <span className="text-sm text-gray-400">{TTD.nik}</span>
+                  </div>
                 </Link>
-                <DeleteInspectionModal selectedInspection={inspection} />
-              </div>
-            </TableCell>
-          </TableRow>
+              </TableCell>
+              {
+                Object.values(TTD.monthlyStatus).map((status, index) => (
+                  <TableCell className="text-center" key={index}>
+                    {status ? <Check className="h-4 w-4 text-green-500" /> : <X className="h-4 w-4 text-red-400" />}
+                  </TableCell>
+                ))
+              }
+            </TableRow>
+          </>
         ))}
       </TableBody>
     </Table>
--- src/app/dashboard/ttd/page.tsx
+++ src/app/dashboard/ttd/page.tsx
@@ -3,23 +3,23 @@ import { Link } from "next-view-transitions";
 import { Button } from "@/components/ui/button";
 import TTDListTable from "components/TTD/TTDListTable";
 
-import useInspectionList from "components/Inspection/hooks/useInspectionList";
+import useTTDList from "components/TTD/hooks/useTTDList";
 
 import { PlusIcon } from "lucide-react";
 
 const TTDList = async () => {
-  const inspections = await useInspectionList();
+  const TTDs = await useTTDList();
 
   return (
     <div className="grid gap-4 px-2 lg:px-6">
       <h1
         className="text-2xl font-semibold text-center lg:text-left text-gray-600"
       >
-        Pemantauan Remaja Putri Meminum TTD (Tablet Tambah Darah)
+        Pemantauan TTD Remaja Putri
       </h1>
 
       <div className="w-full lg:flex lg:justify-end">
-        <Link href={"/dashboard/hasil-pemeriksaan/form"}>
+        <Link href={"/dashboard/ttd/form"}>
           <Button variant={"outline"}
             className="w-full lg:w-fit flex gap-2 rounded-lg text-gray-600"
           >
@@ -29,7 +29,7 @@ const TTDList = async () => {
         </Link>
       </div>
 
-      <TTDListTable inspections={inspections} />
+      <TTDListTable TTDs={TTDs} />
     </div>
   )
 }
