263e7c0c2dd4ca7738d66d01c5b064275f6df16f
syahrilhanla <syahrilhanla5@gmail.com>
2024-04-27 12:58:26 +0700

    refactor: remove "laporan" from the sub menus

--- lib/sidebarLinks.tsx
+++ lib/sidebarLinks.tsx
@@ -12,7 +12,6 @@ export const sidebarLinks = [
   { href: '/dashboard/hasil-pemeriksaan', text: 'Hasil Pemeriksaan', icon: <BarChart2 className="h-5 w-5" /> },
   { href: '/dashboard/ttd', text: 'Tablet Tambah Darah', icon: <HeartPulseIcon className="h-5 w-5" /> },
   { href: '/dashboard/konsultasi', text: 'Konsultasi', icon: <Speech className="h-5 w-5" /> },
-  { href: '/dashboard/laporan', text: 'Laporan', icon: <ClipboardPlus className="h-5 w-5" /> },
   { href: '/dashboard/materi', text: 'Materi Penyuluhan', icon: <BookMarked className="h-5 w-5" /> },
   { href: '/dashboard/profil', text: 'Profil', icon: <Users className="h-5 w-5" /> },
   { href: '/dashboard/pengaturan', text: 'Pengaturan Admin', icon: <UserCog className="h-5 w-5" /> }
