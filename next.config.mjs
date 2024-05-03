1e1fc834a9aa22c7611c60cad62263533c76236a
syahrilhanla <syahrilhanla5@gmail.com>
2024-05-03 23:17:18 +0700

    chore (next.config): add env to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    PROD_HOST: process.env.PROD_HOST,
+    DEV_HOST: process.env.DEV_HOST,
+  },
+};
 
 export default nextConfig;
