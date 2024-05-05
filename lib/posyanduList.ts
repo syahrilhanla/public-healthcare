73e839625657dd896f9ca51154a4aa5cb05651c9
syahrilhanla <syahrilhanla5@gmail.com>
2024-05-05 11:19:34 +0700

    chore (variable): add posyanduList to lib directory

---
+++ lib/posyanduList.ts
@@ -0,0 +1,8 @@
+const posyanduList = [
+  "Posyandu Remaja FRESH",
+  "Posyandu Remaja Smart Gemilang",
+  "Posyandu Remaja Kusuma Jaya",
+  "Posyandu Remaja Mandiri Sehat",
+]
+
+export default posyanduList;
