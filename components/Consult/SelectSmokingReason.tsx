3f28b754b58cc8af5564344dcb8298a42a5190c0
syahrilhanla <syahrilhanla5@gmail.com>
2024-06-30 19:14:42 +0700

    feat: create smoking reason radio button components

--- components/Consult/SelectSmokingReason.tsx
+++ components/Consult/SelectSmokingReason.tsx
@@ -1,17 +1,5 @@
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue
-} from "@/components/ui/select";
-
-import {
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel
-} from "@/components/ui/form";
+import { FormLabel } from "@/components/ui/form";
+import RadioButtons from "components/RadioButtons";
 
 import { SmokingReason } from "lib/reusableValues";
 
@@ -21,41 +9,33 @@ interface Props {
 
 const SelectSmokingReason = ({ form }: Props) => {
   return (
-    <FormField
-      control={form.control}
-      name={"smokingReason"}
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel htmlFor="smokingReason">Apa alasan kamu mulai merokok?</FormLabel>
-          <FormControl>
-            <Select
-              onValueChange={field.onChange}
-              defaultValue={field.value}
-            >
-              <SelectTrigger
-                id="posyandu"
-                className="w-full p-2 border border-gray-300 rounded-md 
-                  focus:outline-none focus:ring-0 focus:ring-transparent"
-              >
-                <SelectValue
-                  placeholder={field.value || "Jenis Konsultasi"}
-                  className="w-full space-x-2 focus-visible:ring-transparent"
-                />
-              </SelectTrigger>
-              <SelectContent>
+    <div className="grid gap-1">
+      <FormLabel htmlFor="reasonToQuit">
+        Dari mana kamu tahu tentang merokok?
+      </FormLabel>
+      <span className="ml-4 space-y-2">
+        {
+          Object.entries(SmokingReason).map(([key, value]) => (
+            <RadioButtons
+              formSchema={form}
+              inputId={key}
+              includeError
+              labelText={value}
+              options={[
                 {
-                  Object.entries(SmokingReason).map(([key, value]) => (
-                    <SelectItem key={key} value={value} className="cursor-pointer">
-                      {value}
-                    </SelectItem>
-                  ))
+                  value: "YES",
+                  label: "Ya"
+                },
+                {
+                  value: "NO",
+                  label: "Tidak"
                 }
-              </SelectContent>
-            </Select>
-          </FormControl>
-        </FormItem>
-      )}
-    />
+              ]}
+            />
+          ))
+        }
+      </span>
+    </div>
   )
 }
 
