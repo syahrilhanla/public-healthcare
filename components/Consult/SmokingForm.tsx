092466eb486482502bc1d6f2281b4e1095d37445
syahrilhanla <syahrilhanla5@gmail.com>
2024-07-02 08:18:28 +0700

    feat: conditional render based on hasSmoke field

--- components/Consult/SmokingForm.tsx
+++ components/Consult/SmokingForm.tsx
@@ -28,80 +28,88 @@ const SmokingForm: React.FC<SmokingFormProps> = ({ form }) => {
         ]}
       />
 
-      <ControlledInput
-        formSchema={form}
-        inputId="ageStartSmoking"
-        includeError
-        labelText="Berapa usia kamu mulai merokok?"
-        placeholder="e.g. 12 tahun"
-      />
+      {
+        form.watch("hasSmoke") === "YES" ? (
+          <>
+            <ControlledInput
+              formSchema={form}
+              inputId="ageStartSmoking"
+              includeError
+              labelText="Berapa usia kamu mulai merokok?"
+              placeholder="e.g. 12 tahun"
+            />
 
-      <SelectSmokingReason form={form} />
+            <SelectSmokingReason form={form} />
 
-      <ControlledInput
-        formSchema={form}
-        inputId="smokingSource"
-        includeError
-        labelText="Dari mana kamu tahu tentang merokok?"
-        placeholder="e.g. teman, keluarga, iklan, dll"
-      />
+            <ControlledInput
+              formSchema={form}
+              inputId="smokingSource"
+              includeError
+              labelText="Dari mana kamu tahu tentang merokok?"
+              placeholder="e.g. teman, keluarga, iklan, dll"
+            />
 
-      <ControlledInput
-        formSchema={form}
-        inputId="cigaretteCount"
-        includeError
-        labelText="Berapa jumlah batang rokok yang kamu hisap setiap hari?"
-        placeholder="e.g. teman, keluarga, iklan, dll"
-      />
+            <ControlledInput
+              formSchema={form}
+              inputId="cigaretteCount"
+              includeError
+              labelText="Berapa jumlah batang rokok yang kamu hisap setiap hari?"
+              placeholder="e.g. teman, keluarga, iklan, dll"
+            />
+          </>
+        ) : form.watch("hasSmoke") === "NO" && (
+          <>
+            <RadioButtons
+              formSchema={form}
+              inputId="knowledgeOfSmokingEffect"
+              includeError
+              labelText="Apakah kamu tahu dampak buruk dari merokok?"
+              options={[
+                {
+                  value: "YES",
+                  label: "Tahu"
+                },
+                {
+                  value: "NO",
+                  label: "Tidak Tahu"
+                }
+              ]}
+            />
 
-      <RadioButtons
-        formSchema={form}
-        inputId="knowledgeOfSmokingEffect"
-        includeError
-        labelText="Apakah kamu tahu dampak buruk dari merokok?"
-        options={[
-          {
-            value: "YES",
-            label: "Tahu"
-          },
-          {
-            value: "NO",
-            label: "Tidak Tahu"
-          }
-        ]}
-      />
+            <RadioButtons
+              formSchema={form}
+              inputId="wantingToQuit"
+              includeError
+              labelText="Apakah kamu ada keinginan untuk berhenti merokok?"
+              options={[
+                {
+                  value: "YES",
+                  label: "Ingin"
+                },
+                {
+                  value: "NO",
+                  label: "Tidak Ingin"
+                }
+              ]}
+            />
 
-      <RadioButtons
-        formSchema={form}
-        inputId="wantingToQuit"
-        includeError
-        labelText="Apakah kamu ada keinginan untuk berhenti merokok?"
-        options={[
-          {
-            value: "YES",
-            label: "Ingin"
-          },
-          {
-            value: "NO",
-            label: "Tidak Ingin"
-          }
-        ]}
-      />
+            <ControlledInput
+              formSchema={form}
+              inputId="reasonToQuit"
+              includeError
+              labelText="Apa alasanmu ingin berhenti merokok?"
+            />
 
-      <ControlledInput
-        formSchema={form}
-        inputId="reasonToQuit"
-        includeError
-        labelText="Apa alasanmu ingin berhenti merokok?"
-      />
-
-      <ControlledInput
-        formSchema={form}
-        inputId="quitSupport"
-        includeError
-        labelText="Siapa yang mendukungmu berhenti merokok?"
-        placeholder="e.g. keluarga, teman, dll"
-      />
+            <ControlledInput
+              formSchema={form}
+              inputId="quitSupport"
+              includeError
+              labelText="Siapa yang mendukungmu berhenti merokok?"
+              placeholder="e.g. keluarga, teman, dll"
+            />
+          </>
+        )
+      }
     </>
   )
 }
